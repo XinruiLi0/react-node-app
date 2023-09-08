f01c8c886fbf383a14d937ce34e0d4483a24b620
XinruiLi0 <xinruili3@cmail.carleton.ca>
2023-09-08 15:13:53 -0400

    add config

--- server/app.js
+++ server/app.js
@@ -39,4 +39,14 @@ app.post('/api/world', (req, res) => {
 // user: "uottawabiomedicalsystems@gmail.com", //
 // pass: "@uOttawa5902",
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Server running on the port : ${port}`));
